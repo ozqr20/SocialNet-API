68e01074854dd9b9c4072094f821ed7e9ea61d40
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-10-10 21:30:29 -0400

    user model, server and index.js

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema ({
+    userName: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+    },
+    thoughts: [
+        {
+            // Array of _id values referencing the Thought model
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+    friends: [
+        {
+            // // Array of _id values (self-reference)
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+});
+
+userSchema.virtual('parceroCount').get(function(){
+    return this.friends.length;
+})
+
+const User = model('user', userSchema);
+
+module.exports = User;
