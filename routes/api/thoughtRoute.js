2facde10cf2614e311a5b11bd7b9df100df83093
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-10-15 20:37:42 -0400

    new user partially broken

--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -11,8 +11,8 @@ const{
 } = require('../../controllers/thoughtController');
 
 router.route('/').get(getEveryThought)
-router.route('/:userId').post(createThought);
 router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+router.route('/:userId').post(createThought);
 router.route('/:thoughtId/reactions').post(addReaction);
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 router.use((req, res) => {
-    res.status(404).json(err);
+    res.status(404).send("Error loading page");
 });
 
 module.exports = router;
